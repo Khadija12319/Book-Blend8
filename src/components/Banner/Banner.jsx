e5e6d626823d29d6c3c275b9699ba4a9d9ac1fde
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-03-27 15:26:53 +0600

    banner section responsive done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,10 +5,10 @@ import "../../App.css"
 const Banner = () => {
     return (
         <div>
-            <div className="container mx-auto flex justify-around py-20 bg-[#1313130D] rounded-3xl">
-                <div className="flex flex-col justify-center">
-                    <h1 className="text-[#131313] text-[56px] font-bold leading-[84px] font2">Books to freshen up <br></br>your bookshelf</h1>
-                    <NavLink to="/listed_books"><button className="text-white bg-[#23BE0A] px-7 py-5 rounded-lg mt-12 text-xl font-bold font1">View The List</button></NavLink>
+            <div className="container md:mx-auto mx-5 flex md:flex-row flex-col-reverse justify-around py-20 bg-[#1313130D] rounded-3xl">
+                <div className="flex flex-col justify-center md:items-start items-center">
+                    <h1 className="text-[#131313] md:text-[56px] text-[26px] font-bold md:leading-[84px] leading-[50px] font2 text-center md:text-left md:pt-0 pt-3">Books to freshen up <br></br>your bookshelf</h1>
+                    <NavLink to="/listed_books"><button className="text-white bg-[#23BE0A] md:px-7 px-4 py-3 md:py-5 rounded-lg md:mt-12 mt-7 text-lg md:text-xl md:font-bold font-semibold font1">View The List</button></NavLink>
                 </div>
                 <div>
                     <img src={banner} alt=""  className="h-[395px] w-auto"/>
